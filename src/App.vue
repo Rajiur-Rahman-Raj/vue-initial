ed09184071b8aba126391d128926e562232e54ac
Rajiur Rahman <rajiurrahmanraj2021@gmail.com>
2023-10-16 00:21:03 +0600

    display data from object to array

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
     const title = "Vue Js 3"
     const subtitle = "Start vue js 3 with project live";
 
+    const data = {
+      title: "Vue Js 3",
+      heading: "Start vue js course with Raju vai",
+      date: () => {
+        const date = new Date();
+        return date.toDateString();
+      },
+      task:[1,2,3,4,5]
+    }
+
     function getDate(){
       const date = new Date();
       return date.toDateString();
@@ -11,12 +21,15 @@
 <template>
     <div class="card">
   <div class="card-header">
-    {{ title }}
+    {{ data.title }}
   </div>
   <div class="card-body">
     <blockquote class="blockquote mb-0">
-      <p> {{ subtitle }}</p>
-      <footer class="blockquote-footer">{{ getDate() }} </footer>
+      <h5 class=""> {{ data.heading }}</h5>
+      <!-- <p>You have {{ data.task[2] }} task for today</p> -->
+      <p>You have {{ data.task.length }} task for today</p>
+
+      <footer class="blockquote-footer mt-5">{{ data.date() }} </footer>
     </blockquote>
   </div>
 </div>
