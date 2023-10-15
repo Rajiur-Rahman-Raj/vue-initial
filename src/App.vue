01e3177a71d7aaae54c8620ff53f40b32e1d128d
Rajiur Rahman <rajiurrahmanraj2021@gmail.com>
2023-10-16 00:01:25 +0600

    inital commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+    const title = "Vue Js 3"
+    const subtitle = "Start vue js 3 with project";
+
+    function getDate(){
+      const date = new Date();
+      return date.toDateString();
+    }
+</script>
+
+<template>
+    <div class="card">
+  <div class="card-header">
+    {{ title }}
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p> {{ subtitle }}</p>
+      <footer class="blockquote-footer">{{ getDate() }} </footer>
+    </blockquote>
+  </div>
+</div>
+</template>
+
+<style scoped>
+</style>
