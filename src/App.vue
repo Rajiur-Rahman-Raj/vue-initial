7ea2e493c3b7528f55f06f344239d99ffb3495e4
Rajiur Rahman <rajiurrahmanraj2021@gmail.com>
2023-10-17 00:14:56 +0600

    v-if items grouping with template tag

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 
   const location = "dhaka"
-  const weather = "rainy"
+  const weather = "cloudy"
 
 
 </script>
@@ -9,17 +9,31 @@
 <template>
   <section>
       <h2>Location = {{ location }}</h2>
-      <h2 v-if="weather == 'sunny'">Weather = Sunny</h2>
-      <h2 v-else-if="weather == 'cloudy'">Weather = Cloudy</h2>
-      <h2 v-else-if="weather == 'rainy'">Weather = Rainy</h2>
-      <h2 v-else-if="weather == 'thundarstrom'">Weather = Thundarstrom</h2>
-      <h2 v-else>No weather found</h2>
 
-      <img v-if="weather == 'sunny'" src="./assets/images/sunny.png" alt="" width="200">
-      <img v-else-if="weather == 'cloudy'" src="./assets/images/cloudy.png" alt="" width="200">
-      <img v-else-if="weather == 'rainy'" src="./assets/images/rainy.png" alt="" width="200">
-      <img v-else-if="weather == 'thundarstrom'" src="./assets/images/thunderstorm.png" alt="" width="200">
-      <h4 v-else>No matching </h4>
+      <template v-if="weather == 'sunny'">
+        <h2>Weather = Sunny</h2>
+        <img src="./assets/images/sunny.png" alt="" width="200">
+      </template>
+
+      <template v-else-if="weather == 'cloudy'">
+        <h2>Weather = Cloudy</h2>
+        <img src="./assets/images/cloudy.png" alt="" width="200">
+      </template>
+
+      <template v-else-if="weather == 'rainy'">
+        <h2>Weather = Rainy</h2>
+        <img src="./assets/images/rainy.png" alt="" width="200">
+      </template>
+
+      <template v-else-if="weather == 'thundarstrom'">
+        <h2>Weather = Thundarstrom</h2>
+        <img src="./assets/images/thunderstorm.png" alt="" width="200">
+      </template>
+
+      <template v-else>
+        <h2>Weather = No matching</h2>
+      
+      </template>
   </section>
 </template>
 
