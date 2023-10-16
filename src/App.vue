c547a47aa2397be47dd7de2c8d426f0cb69111b4
Rajiur Rahman <rajiurrahmanraj2021@gmail.com>
2023-10-16 23:59:40 +0600

    Logic If else condition

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <script setup>
 
-  const title1 = "<strong>lorem ipsum dummy text one</strong>";
-  const title2 = "lorem ipsum dummy <em>text one</em>";
+  const location = "dhaka"
+  const weather = "rainy"
+
 
 </script>
 
 <template>
   <section>
-      <h1 v-html="title1"></h1>
-      <h2 v-html="title2"></h2>
+      <h2>Location = {{ location }}</h2>
+      <h2 v-if="weather == 'sunny'">Weather = Sunny</h2>
+      <h2 v-else-if="weather == 'cloudy'">Weather = Cloudy</h2>
+      <h2 v-else-if="weather == 'rainy'">Weather = Rainy</h2>
+      <h2 v-else-if="weather == 'thundarstrom'">Weather = Thundarstrom</h2>
+      <h2 v-else>No weather found</h2>
+
+      <img v-if="weather == 'sunny'" src="./assets/images/sunny.png" alt="" width="200">
+      <img v-else-if="weather == 'cloudy'" src="./assets/images/cloudy.png" alt="" width="200">
+      <img v-else-if="weather == 'rainy'" src="./assets/images/rainy.png" alt="" width="200">
+      <img v-else-if="weather == 'thundarstrom'" src="./assets/images/thunderstorm.png" alt="" width="200">
+      <h4 v-else>No matching </h4>
   </section>
 </template>
 
