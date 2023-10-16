079af5743f69a3b6ba126cf4a403e6c8f5acdd4d
Rajiur Rahman <rajiurrahmanraj2021@gmail.com>
2023-10-16 22:49:32 +0600

    full object pass in one tag using attribute

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   import {
     data
   } from "./data/data"
+
   const teams = [{
       name: "John Doe",
       designation: "Web Designer",
@@ -10,23 +11,34 @@
     },
     {
       name: "Jane Doe",
-      designation: "Web Designer",
+      designation: "Web Developer",
       image: "https://source.unsplash.com/random/300x200?2",
       profileLink: "https://www.pexels.com/photo/pink-flower-in-close-up-photography-8311903/"
     },
     {
       name: "Jiniya Doe",
-      designation: "Web Designer",
+      designation: "UI/UX Designer",
       image: "https://source.unsplash.com/random/300x200?3",
       profileLink: "https://www.pexels.com/photo/landscape-photography-of-the-river-seine-13988101/"
     },
   ];
+
   const singleMember = {
     name: "John Doe",
-    designation: "Web Designer",
+    designation: "Flatter Developer",
     image: "https://source.unsplash.com/random/300x200?1",
     profileLink: "https://unsplash.com/photos/a-vase-filled-with-lots-of-flowers-on-top-of-a-table-LTw2ld-5q4M"
   }
+
+  const finalMember = {
+    title:"Raj Sarkar profile",
+    src:"https://source.unsplash.com/random/300x200?3",
+    alt: "John doe image",
+    name: "Raj Sarkar", 
+    designation: "Full Stack Developer", 
+    profileLink: "https://www.pexels.com/photo/landscape-photography-of-the-river-seine-13988101/", 
+  }
+
 </script>
 
 <template>
@@ -61,9 +73,9 @@
             <div class="card" style="width:20rem;margin:20px 0 24px 0">
               <img class="card-img-top" :src="teams[2].image" alt="image" style="width:100%">
               <div class="card-body">
-                <h4 class="card-title">{{ teams[1].name }}</h4>
-                <p class="card-text">{{ teams[1].designation }}</p>
-                <a :href="teams[1].profileLink" class="btn btn-primary">See Profile</a>
+                <h4 class="card-title">{{ teams[2].name }}</h4>
+                <p class="card-text">{{ teams[2].designation }}</p>
+                <a :href="teams[2].profileLink" class="btn btn-primary">See Profile</a>
               </div>
             </div>
           </div>
@@ -77,6 +89,20 @@
               <div class="card-body">
                 <h4 class="card-title">{{ singleMember.name }}</h4>
                 <p class="card-text">{{ singleMember.designation }}</p>
+                <a :href="singleMember.profileLink" class="btn btn-primary">See Profile</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="text-center">Full Object Passing</h2>
+            <div class="card" style="width:20rem;margin:20px 0 24px 0">
+              <img class="card-img-top" v-bind="finalMember" style="width:100%">
+              <div class="card-body">
+                <h4 class="card-title">{{ finalMember.name }}</h4>
+                <p class="card-text">{{ finalMember.designation }}</p>
                 <a :href="singleMember.profileLink" class="btn btn-primary">See Profile</a>
               </div>
             </div>
