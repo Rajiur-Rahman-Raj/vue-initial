b4191d44df3bf75abf960feee253ef356223ecc0
Rajiur Rahman <rajiurrahmanraj2021@gmail.com>
2023-10-17 00:37:05 +0600

    difference between v-if and v-show

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
+import { ref } from "vue"
+
 
   const location = "dhaka"
-  const weather = "cloudy"
+  const weather = ref("sunny")
 
+
+  // document.getElementById('sunny').addEventListener('click', function(){
+  //   alert('sunny weather');
+  // })
 
 </script>
 
@@ -10,9 +16,9 @@
   <section>
       <h2>Location = {{ location }}</h2>
 
-      <template v-if="weather == 'sunny'">
+      <!-- <template v-if="weather == 'sunny'">
         <h2>Weather = Sunny</h2>
-        <img src="./assets/images/sunny.png" alt="" width="200">
+        <img src="./assets/images/sunny.png" alt="" id="sunny" width="200">
       </template>
 
       <template v-else-if="weather == 'cloudy'">
@@ -32,8 +38,35 @@
 
       <template v-else>
         <h2>Weather = No matching</h2>
+      </template> -->
+
+
+
+      <div v-show="weather == 'sunny'">
+        <h2>Weather = Sunny</h2>
+        <img src="./assets/images/sunny.png" alt="" id="sunny" width="200">
+      </div>
+
+      <div v-show="weather == 'cloudy'">
+        <h2>Weather = Cloudy</h2>
+        <img src="./assets/images/cloudy.png" alt="" width="200">
+      </div>
+
+      <div v-show="weather == 'rainy'">
+        <h2>Weather = Rainy</h2>
+        <img src="./assets/images/rainy.png" alt="" width="200">
+      </div>
+
+      <div v-show="weather == 'thundarstrom'">
+        <h2>Weather = Thundarstrom</h2>
+        <img src="./assets/images/thunderstorm.png" alt="" width="200">
+      </div>
+
+  
+      <form>
+        <input type="text" class="form-control" v-model="weather">
+      </form>
       
-      </template>
   </section>
 </template>
 
